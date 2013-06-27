2afc87c06b5d86640bfb8c28a1928ddf17838494
April Gordon <april.k.gordon@gmail.com>
2013-06-26 21:29:24 -0700

    Adding some files from april's local machine

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.marineelectronics4less.com/');
+define('HTTP_IMAGE', 'http://www.marineelectronics4less.com/image/');
+define('HTTP_ADMIN', 'http://www.marineelectronics4less.com/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://www.marineelectronics4less.com/');
+define('HTTPS_IMAGE', 'https://www.marineelectronics4less.com/image/');
+
+// DIR
+define('DIR_APPLICATION', '/home/marine/public_html/catalog/');
+define('DIR_SYSTEM', '/home/marine/public_html/system/');
+define('DIR_DATABASE', '/home/marine/public_html/system/database/');
+define('DIR_LANGUAGE', '/home/marine/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/home/marine/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/home/marine/public_html/system/config/');
+define('DIR_IMAGE', '/home/marine/public_html/image/');
+define('DIR_CACHE', '/home/marine/public_html/system/cache/');
+define('DIR_DOWNLOAD', '/home/marine/public_html/download/');
+define('DIR_LOGS', '/home/marine/public_html/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'marine_OCadmin');
+define('DB_PASSWORD', 'Temp457Stuff%');
+define('DB_DATABASE', 'marine_OCmain');
+define('DB_PREFIX', '');
+?>
